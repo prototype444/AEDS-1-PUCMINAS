4e3011829a70c8a55c25c281f6803ed3f2c03d71
prototype444 <117061014+prototype444@users.noreply.github.com>
2024-03-12 11:33:28 -0300

    Add files via upload

---
+++ triangulo.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+int main(){
+int a,b,c;
+scanf("%d",&a);
+scanf("%d",&b);
+scanf("%d",&c);
+
+if(a+b>c && a-b<c){
+    printf("pode formar um triangulo");
+}else{
+    printf("nao pode formar um triangulo");
+}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
