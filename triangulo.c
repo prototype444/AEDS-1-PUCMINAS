d9be5d0aea5f0f815c529eec181c22fdb336a861
prototype444 <117061014+prototype444@users.noreply.github.com>
2024-03-12 11:51:14 -0300

    Update triangulo.c

--- triangulo.c
+++ triangulo.c
@@ -7,21 +7,19 @@ scanf("%d",&c);
 
 if(a+b>c && a-b<c){
     printf("pode formar um triangulo");
-}else{
+    if(a!=b && a!=c && c!=b){
+        printf("\nescaleno");
+    }else if  (a==b && a==c && b==c){
+        printf("\nequilatero");
+    }
+    else if(a==b || a==c || b==c){
+        printf("\nisosceles");
+    }
+}
+
+
+
+else{
     printf("nao pode formar um triangulo");
 }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
